6bf11fb312debdb1c33378e24428d82ed7ab1bdc
Henry Tran <khang241010@gmail.com>
2025-01-06 21:14:33 +0700

    help + command permissions

--- main.py
+++ main.py
@@ -35,31 +35,34 @@ async def loadbms(ctx, bms):
     
 @client.command("loadbms_intodatabase")
 async def loadbmsintodatabase(ctx, msid):
-    bms = None
-    
-    json_object = None
-    
-    file = open("maps.json", "r")
-    json_object = json.load(file)
-    file.close()
-    
-    try:
-        bms = json_object[str(msid)]
-    except:
-        json_object[str(msid)] = load_beatmapset(msid)
-        file = open("maps.json", "w")
-        json.dump(json_object, file)
+    if ctx.author.id == 718102801242259466:
+        bms = None
+        
+        json_object = None
+        
+        file = open("maps.json", "r")
+        json_object = json.load(file)
         file.close()
         
-        bms = json_object[str(msid)]
-        
-        
-        await ctx.message.reply(f"Beatmap {bms["title"]} of ID {bms["id"]} has been loaded into the database.")
-    else:
-        bms = json_object[str(msid)]
-        
-        await ctx.message.reply(f"Beatmap {bms["title"]} of ID {bms["id"]} has already been loaded.")
-        return
+        try:
+            bms = json_object[str(msid)]
+        except:
+            json_object[str(msid)] = load_beatmapset(msid)
+            file = open("maps.json", "w")
+            json.dump(json_object, file)
+            file.close()
+            
+            bms = json_object[str(msid)]
+            
+            
+            await ctx.message.reply(f"Beatmap {bms["title"]} of ID {bms["id"]} has been loaded into the database.")
+        else:
+            bms = json_object[str(msid)]
+            
+            await ctx.message.reply(f"Beatmap {bms["title"]} of ID {bms["id"]} has already been loaded.")
+            return
+    
+    await ctx.message.reply("You do not have the permission to use this command.")  
     
 
 def loadbms(msid):
@@ -88,19 +91,25 @@ def loadbms(msid):
 
 @client.command("load_next")
 async def loadnext_page(ctx):
-    loadnpage()
+    if ctx.author.id == 718102801242259466:
+        loadnpage()
+        
+        await ctx.message.reply("Loaded 50 new beatmaps into the database")
     
-    await ctx.message.reply("Loaded 50 new beatmaps into the database")
+    await ctx.message.reply("You do not have the permission to use this command.")  
     
 @client.command("load_multipages")
 async def loadmanypages(ctx, num):
-    amount_maps = 0
+    if ctx.author.id == 718102801242259466:
+        amount_maps = 0
+        
+        for i in range(int(num)):
+            loadnpage()
+            amount_maps += 50
+        
+        await ctx.message.reply(f"{amount_maps} maps has been loaded!")
     
-    for i in range(int(num)):
-        loadnpage()
-        amount_maps += 50
-    
-    await ctx.message.reply(f"{amount_maps} maps has been loaded!")
+    await ctx.message.reply("You do not have the permission to use this command.")  
     
 @client.command("mapsloaded")
 async def loadedamount(ctx):
@@ -112,15 +121,21 @@ async def loadedamount(ctx):
     
 @client.command("load_diffs_sorted")
 async def load_nmz_diffs(ctx):
-    add_diffs_to_sorted_file()
+    if ctx.author.id == 718102801242259466:
+        add_diffs_to_sorted_file()
+        
+        await ctx.message.reply("Done.")  
     
-    await ctx.message.reply("Done.")    
+    await ctx.message.reply("You do not have the permission to use this command.")  
     
 @client.command("load_normalized_diffs")
 async def load_nmz_diffs(ctx):
-    add_normalized_diffs_to_sorted_file()
+    if ctx.author.id == 718102801242259466:
+        add_normalized_diffs_to_sorted_file()
+        
+        await ctx.message.reply("Done.")
     
-    await ctx.message.reply("Done.")
+    await ctx.message.reply("You do not have the permission to use this command.")
 
 @client.command("clear_all_maps")
 async def clear_maps_cmd(ctx):
@@ -130,6 +145,7 @@ async def clear_maps_cmd(ctx):
         file.close()
         
         await ctx.message.reply(f"This is a big decision. Are you sure about this? You have 20 seconds to turn off the bot before {len(json_object)} maps gets cleared")
+        print(f"This is a big decision. Are you sure about this? You have 20 seconds to turn off the bot before {len(json_object)} maps gets cleared")
         
         await asyncio.sleep(20)
         
@@ -156,6 +172,7 @@ async def clear_sorted_diffs_cmd(ctx):
         file.close()
         
         await ctx.message.reply(f"This is a big decision. Are you sure about this? You have 20 seconds to turn off the bot before {len(json_object)} difficulties gets cleared")
+        print(f"This is a big decision. Are you sure about this? You have 20 seconds to turn off the bot before {len(json_object)} difficulties gets cleared")
         
         await asyncio.sleep(20)
         
@@ -168,5 +185,18 @@ async def clear_sorted_diffs_cmd(ctx):
         return
     
     await ctx.message.reply("You do not have the permission to use this command.")
+    
+@client.command("help")
+async def help(ctx):
+    await ctx.message.reply("Check DMs.")
+    
+    user=await client.get_user_info(ctx.author.id)
+    await client.send_message(user, """Prefix - o!
+ping - Check status of Bot. example: o!ping.
+calculaterarity - Calculate rarity of a given star rating. Arguments: <sr>. example: o!calculaterarity 7.86.
+load_beatmapset - Returns json data of a beatmapset with a given bms id. Arguments: <bms_id>. example: o!load_beatmapset 2288709.
+mapsloaded - Check how many maps has been loaded into its database. example: o!mapsloaded.
+help - Shows this message.
+7 more dev-only commands.""")
 
 client.run(os.getenv("token"))
